75a795f013a63da934973b1801e45d988ac02cae
Kiryl Bokiy <kiryl.bokiy@jnet.nl>
2023-09-24 00:37:00 +0300

    spring boot init commit

---
+++ src/main/java/lt/beltechvillage/ksc/KscApplication.java
@@ -0,0 +1,13 @@
+package lt.beltechvillage.ksc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/beltechvillage/ksc/KscApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.beltechvillage.ksc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
