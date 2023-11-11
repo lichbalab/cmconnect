43d7f2cde31005cc435a2d6c6b60418b6fabe89e
Kiryl Bokiy <kiryl.bokiy@gmail.com>
2023-11-11 15:56:40 +0200

    Consuming REST API by react UI module

--- modules/ui/src/main/ui/src/services/api.js
+++ modules/ui/src/main/ui/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const API_BASE_URL = 'http://your-api-url.com/api';
+const API_BASE_URL = 'http://localhost:8080';
 
 export const getCertificates = () => {
     return axios.get(`${API_BASE_URL}/certificates`);
