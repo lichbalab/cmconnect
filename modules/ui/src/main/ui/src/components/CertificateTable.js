2c4c84f4705932a4a68966be79030ba0c678b1fb
Kiryl Bokiy <kiryl.bokiy@gmail.com>
2023-11-21 02:16:15 +0200

    Upload button added

--- modules/ui/src/main/ui/src/components/CertificateTable.js
+++ modules/ui/src/main/ui/src/components/CertificateTable.js
@@ -1,10 +1,12 @@
 
 import React, { useState, useEffect } from 'react';
-import { getCertificates, deleteCertificate } from '../services/api';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button } from '@mui/material';
+import { getCertificates, deleteCertificate, uploadCertificate } from '../services/api';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Button, TextField } from '@mui/material';
 
 const CertificateTable = () => {
     const [certificates, setCertificates] = useState([]);
+    const [selectedFile, setSelectedFile] = useState(null);
+    const [alias, setAlias] = useState(''); // State for the certificate alias
 
     useEffect(() => {
         loadCertificates();
@@ -20,42 +22,74 @@ const CertificateTable = () => {
         loadCertificates();
     };
 
+    const handleFileUpload = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleAliasChange = (event) => {
+        setAlias(event.target.value);
+    };
+
+    const uploadFile = async () => {
+        if (!selectedFile || alias.trim() === '') {
+            alert("Please select a file and enter an alias!");
+            return;
+        }
+
+        // Assuming 'uploadCertificate' is the function from your api service to upload the file
+        try {
+            await uploadCertificate(selectedFile, alias);
+            setAlias('');
+            loadCertificates(); // Reload certificates after successful upload
+        } catch (error) {
+            console.error("Error uploading file:", error);
+            // Handle the error appropriately
+        }
+    };
+
      return (
-        <TableContainer component={Paper} style={{ width: '100%', height: '100%' }}>
-          <h2 style={{ textAlign: 'center' }}>Certificates</h2>
-          <Table>
-            <TableHead>
-              <TableRow>
-                <TableCell>Alias</TableCell>
-                <TableCell>Subject</TableCell>
-                <TableCell>Issuer</TableCell>
-                <TableCell>Serial Number</TableCell>
-                <TableCell>Expiration Timestamp</TableCell>
-                <TableCell>Action</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {certificates.map((row) => (
-                <TableRow key={row.alias}>
-                  <TableCell>{row.alias}</TableCell>
-                  <TableCell>{row.subject}</TableCell>
-                  <TableCell>{row.issuer}</TableCell>
-                  <TableCell>{row.serialNumber}</TableCell>
-                  <TableCell>{row.expirationTimestamp}</TableCell>
-                  <TableCell>
-                    <Button
-                      variant="contained"
-                      color="secondary"
-                      onClick={() => handleDelete(row.id)}
-                    >
-                      Remove
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <div style={{ width: '100%', height: '100%' }}>
+            <h2 style={{ textAlign: 'center' }}>Certificates</h2>
+            <TableContainer component={Paper} style={{ width: '100%', height: '100%' }}>
+              <TextField label="Alias" value={alias} onChange={handleAliasChange} required />
+              <input type="file" onChange={handleFileUpload} />
+                <Button onClick={uploadFile} variant="contained" color="primary">
+                    Upload Certificate
+                </Button>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Alias</TableCell>
+                    <TableCell>Subject</TableCell>
+                    <TableCell>Issuer</TableCell>
+                    <TableCell>Serial Number</TableCell>
+                    <TableCell>Expiration Timestamp</TableCell>
+                    <TableCell>Action</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {certificates.map((row) => (
+                    <TableRow key={row.alias}>
+                      <TableCell>{row.alias}</TableCell>
+                      <TableCell>{row.subject}</TableCell>
+                      <TableCell>{row.issuer}</TableCell>
+                      <TableCell>{row.serialNumber}</TableCell>
+                      <TableCell>{row.expirationTimestamp}</TableCell>
+                      <TableCell>
+                        <Button
+                          variant="contained"
+                          color="secondary"
+                          onClick={() => handleDelete(row.id)}
+                        >
+                          Remove
+                        </Button>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+        </div>
       );
     };
 
--- modules/ui/src/main/ui/src/services/api.js
+++ modules/ui/src/main/ui/src/services/api.js
@@ -10,3 +10,21 @@ export const getCertificates = () => {
 export const deleteCertificate = (id) => {
     return axios.delete(`${API_BASE_URL}/certificates/${id}`);
 };
+
+
+export const uploadCertificate = (file, alias) => {
+    try {
+        const formData = new FormData();
+        formData.append('file', file); // Adjust the 'file' key according to your API requirements
+
+        return axios.post(`${API_BASE_URL}/certificates/upload?alias=${encodeURIComponent(alias)}`, formData, {
+            headers: {
+              'Content-Type': 'multipart/form-data'
+            }
+          });
+          // Additional logic after successful upload
+    } catch (error) {
+      console.error('Error uploading file:', error);
+      // Handle errors here
+    }
+};
