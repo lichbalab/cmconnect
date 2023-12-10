4fd8c254f093edab13c85f121e65584dfa2b927b
Kiryl Bokiy <kiryl.bokiy@gmail.com>
2023-12-11 01:02:50 +0200

    Minor fixes of Signing PDF API

--- modules/ksc-document/src/main/java/com/lichbalab/ksc/doc/DocSignService.java
+++ modules/ksc-document/src/main/java/com/lichbalab/ksc/doc/DocSignService.java
@@ -2,18 +2,14 @@ package com.lichbalab.ksc.doc;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
-import java.security.cert.CertificateException;
 
-import com.lichbalab.certificate.Certificate;
 import eu.europa.esig.dss.model.DSSDocument;
-import org.bouncycastle.openssl.PEMException;
 
 public interface DocSignService {
     /**
      *
      * @param doc The document to be signed
-     * @param certificate Certificate for signature.
+     * @param certAlias alias of Certificate for signature.
      * @return Signed document
      */
     DSSDocument signPdf(InputStream doc, String certAlias) throws IOException;
