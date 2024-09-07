ec971854090d23933411ee1cb3b3785d2eb44fb7
lichbalab <kiryl.bokiy@gmail.com>
2024-09-07 17:45:50 +0300

    cmc-doc module removed

--- modules/cmc-document/src/main/java/com/lichbalab/cmc/doc/SignatureValidationServiceLLab.java
+++
@@ -1,8 +0,0 @@
-package com.lichbalab.cmc.doc;
-
-import eu.europa.esig.dss.ws.validation.dto.WSReportsDTO;
-
-public interface SignatureValidationServiceLLab {
-
-    WSReportsDTO validateSignature(byte[] signedDocument);
-}
